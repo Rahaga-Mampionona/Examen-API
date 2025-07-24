9ed191cfc3c6c38269d150a20381608d9626c150
Rahaga-Mampionona <hei.mampionona.5@gmail.com>
2025-07-24 08:55:11 +0300

    Ajout de la réponse à la deuxième question

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
+from fastapi import FastAPI, Query, status
+from fastapi.responses import PlainTextResponse
+from pydantic import BaseModel
+from typing import List, Optional
 from fastapi import FastAPI, Query
 from typing import Optional
 
 app = FastAPI()
 
+students_db = []
+
+#@app.get("/")
+#async def root():
+#  return {"message": "Hello world"}
+
+#@app.get("/hello/{name}")
+#async def say_hello(name: str):
+#   return {"message": f"Hello {name}"}
+
 @app.get("/")
 async def root():
-    return {"message": "Hello world"}
+  return {"message": "Welcome Mampionona"}
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/welcome", status_code=200)
+async def welcome_user(name: str = Query(..., description="Mampionona")):
+    return {"message": f"Welcome {name}"}
+
+
+class Student(BaseModel):
+    Reference: str
+    FirstName: str
+    LastName: str
+    Age: int
+
+
+
+
+
+
+
+
